28345d6b65d447f13b41a5eb3c9e08cc283f01d8
Sleya <yehiazz98246@abv.bg>
2018-02-11 19:44:29 +0200

    final exam

---
+++ CSharp-Advanced/Exa/Exam/KeyResolver/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyResolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+            var bullets = Console.ReadLine().Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var locks = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int intelligenceValue = int.Parse(Console.ReadLine());
+            int originalBulletAmount = bullets.Count;
+            int counter = 0;
+            bool safeBroke = true;
+            for (int bul = (bullets.Count - 1); bul >=0 ; bul--)
+            {
+                if (bullets.Count == 0)
+                {
+                    safeBroke = false;
+                    break;
+                }
+                for (int lockk = 0; lockk < locks.Count; lockk++)
+                {
+
+                    if (bullets[bul] <= locks[lockk])
+                    {
+                        Console.WriteLine($"Bang!");
+                        locks.RemoveAt(lockk);
+                        bullets.RemoveAt(bul);
+                        lockk--;
+                        bul--;
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"Ping!");
+                        bullets.RemoveAt(bul);
+                        bul--;
+                        lockk--;
+
+                    }
+
+                    counter++;
+                    if (bullets.Count==0)
+                    {
+                        safeBroke = false;
+                        break;
+                    }
+                    if (counter == gunBarrelSize && bullets.Count > 0)
+                    {
+                        Console.WriteLine("Reloading!");
+                        counter = 0;
+                    }
+                }
+                
+            }
+
+            
+            if (!safeBroke && locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                int moneyEarned = intelligenceValue - (bulletPrice * (originalBulletAmount - bullets.Count));
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+            }
+        }
+    }
+}
