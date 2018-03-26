bd5962f0c8e4c8d6f8d7831baf0b7b7d9ebc0d51
Sleya <yehiazz98246@abv.bg>
2018-03-26 18:02:00 +0300

    Removed GrandPrix exam

--- Exam - GrandPrix/GrandPrix/Models/Drivers/AggressiveDriver.cs
+++
@@ -1,9 +0,0 @@
-﻿public class AggressiveDriver : Driver
-{
-    public AggressiveDriver(string name, Car car)
-        : base(name, car)
-    {
-        base.FuelConsumptionPerKm = 2.7;
-        base.Speed *= 1.3;
-    }
-}
--- Exam - GrandPrix/GrandPrix/StartUp.cs
+++
@@ -1,8 +0,0 @@
-﻿class StartUp
-{
-    static void Main(string[] args)
-    {
-        Engine engine = new Engine();
-        engine.Run();
-    }
-}
